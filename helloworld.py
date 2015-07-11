8b7b1187abd3f4b29c0fbe46b104b00cdafaec0b
b11005 <instantjoy3@gmail.com>
2015-07-11 17:52:28 +0900

    tutorial, user login

--- helloworld.py
+++ helloworld.py
@@ -1,3 +1,4 @@
+from google.appengine.api import users 
 import webapp2
 
 class MainPage(webapp2.RequestHandler):
@@ -7,9 +8,16 @@ class MainPage(webapp2.RequestHandler):
 
 class Say(webapp2.RequestHandler):
 	def get(self):
-		self.response.write("Say! Hello world")
+		user=users.get_current_user()
+		if user:
+			self.response.headers['Content-Type'] = 'text/html; charset=utf-8'
+			self.response.write('hello, '+ user_nickname())
+		else:
+			self.redirect(users.create_login_url(self.request.uri))
+		self.response.write('guest')
+		self.response.write("Say! Hello world!")
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
-    ("/say", Say)
+    ('/say', Say)
 ], debug=True)

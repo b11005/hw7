cfeedb846a8876801b86c77e80fd32a91c652494
b11005 <instantjoy3@gmail.com>
2015-07-11 15:22:01 +0900

    say

--- helloworld.py
+++ helloworld.py
@@ -5,6 +5,11 @@ class MainPage(webapp2.RequestHandler):
         self.response.headers['Content-Type'] = 'text/plain'
         self.response.write('Hello, World!')
 
+class Say(webapp2.RequestHandler):
+	def get(self):
+		self.response.write("Say! Hello world")
+
 app = webapp2.WSGIApplication([
     ('/', MainPage),
+    ("/say", Say)
 ], debug=True)

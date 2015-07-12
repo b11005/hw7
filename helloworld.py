21d08b5bf401d84edec2a6042d53a058f70b7a4e
b11005 <instantjoy3@gmail.com>
2015-07-12 13:41:58 +0900

    hello world

--- helloworld.py
+++ helloworld.py
@@ -1,5 +1,5 @@
 from google.appengine.api import users 
-import webapp2
+import webapp2,random
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
@@ -8,16 +8,22 @@ class MainPage(webapp2.RequestHandler):
 
 class Say(webapp2.RequestHandler):
 	def get(self):
-		user=users.get_current_user()
+		'''user=users.get_current_user()
 		if user:
 			self.response.headers['Content-Type'] = 'text/html; charset=utf-8'
 			self.response.write('hello, '+ user_nickname())
 		else:
-			self.redirect(users.create_login_url(self.request.uri))
-		self.response.write('guest')
+			self.redirect(users.create_login_url(self.request.uri))'''
+		self.response.write('guest  ')
 		self.response.write("Say! Hello world!")
+
+class Animal(webapp2.RequestHandler):
+	def get(self):
+		animal=['lemur','weasel','anteater','kangaroo','giraffe','tiger','baboon','leopard','sheep','camel']
+		self.response.write(animal[random.randint(0,9)])
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
-    ('/say', Say)
+    ('/say', Say),
+    ('/animal',Animal)
 ], debug=True)
